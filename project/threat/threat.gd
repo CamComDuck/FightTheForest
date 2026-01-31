d97d1e752f7feeda8144663b96cb1b18057b14d3
Camden Hovell <camdenhovell@gmail.com>
2026-01-31 11:22:46 -0500

    Add UI and images to fight scene

---
+++ project/threat/threat.gd
@@ -0,0 +1,13 @@
+@icon("res://threat/tree.png")
+class_name Threat
+extends Sprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
