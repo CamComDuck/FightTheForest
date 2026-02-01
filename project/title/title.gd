be0198e93fe59bae0c850ea0fbd9e5a93e20a973
Camden Hovell <camdenhovell@gmail.com>
2026-02-01 12:52:21 -0500

    Add title scene layout

---
+++ project/title/title.gd
@@ -0,0 +1,18 @@
+class_name Title
+extends Node2D
+
+
+func _on_start_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_input_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_story_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_credits_button_pressed() -> void:
+	pass # Replace with function body.
